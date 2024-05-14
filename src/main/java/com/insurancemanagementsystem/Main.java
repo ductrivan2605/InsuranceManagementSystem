f89238bf0cd58e9e666f93d2dbbcee01aa441204
s3836565 <62092164+s3836565@users.noreply.github.com>
2024-05-14 13:40:21 +0700

    testing new UI draft

--- src/main/java/com/insurancemanagementsystem/Main.java
+++ src/main/java/com/insurancemanagementsystem/Main.java
@@ -3,11 +3,14 @@ package com.insurancemanagementsystem;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
+    public Label welcomeText; // tessingf for UI
+
     @Override
     public void start(Stage stage) {
         try {
